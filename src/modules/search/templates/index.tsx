cb103c045f9f6b95c41bcd96a806587acf16dd6f
yspv <yusupovamirxon@yahoo.com>
2025-12-27 08:26:07 +0500

    search template

--- src/modules/search/templates/index.tsx
+++ src/modules/search/templates/index.tsx
@@ -18,14 +18,12 @@ export default function SearchPageTemplate(props: {
     <>
       <RefinementList categories={categories} />
       <div className="flex-1 min-w-0">
-        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          <PaginatedProducts
-            locale={locale}
-            page={pageNumber}
-            sortBy={sort}
-            q={q}
-          />
-        </div>
+        <PaginatedProducts
+          locale={locale}
+          page={pageNumber}
+          sortBy={sort}
+          q={q}
+        />
       </div>
     </>
   );

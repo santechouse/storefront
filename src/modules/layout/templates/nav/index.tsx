10462fb53450803b280c84e059b32329b7a03542
yspv <yusupovamirxon@yahoo.com>
2025-12-27 08:25:05 +0500

    add mobile search in nav

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -1,14 +1,20 @@
-import { Input } from "@/components/ui/input";
+"use client";
 import { Link } from "@/i18n/navigation";
-import { ShoppingCart, UserIcon } from "lucide-react";
-import { useTranslations } from "next-intl";
+import { SearchIcon, ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
+import React from "react";
+import { SearchInput } from "../../components/search-input";
+import { MobileSearchInput } from "../../components/mobile-search-input";
+import { Button } from "@/components/ui/button";
 
 export default function Nav() {
-  const t = useTranslations("Nav");
+  const [isMobileSearch, setMobileSearch] = React.useState(false);
+  if (isMobileSearch) {
+    return <MobileSearchInput onClose={() => setMobileSearch(false)} />;
+  }
   return (
-    <div className=" sticky top-0 z-50 w-full px-4 py-4 md:px-10 lg:px-20 flex justify-center backdrop-blur-md bg-white/80 dark:bg-background/80 border-b border-slate-200 dark:border-[#282e39]/50">
-      <div className="flex w-full max-w-7xl items-center justify-between gap-4">
+    <div className=" sticky top-0 z-50 w-full px-4 py-4 md:px-6 lg:px-8 flex justify-center backdrop-blur-md bg-white/80 dark:bg-background/80 border-b border-slate-200 dark:border-[#282e39]/50">
+      <div className="flex w-full items-center justify-between gap-4">
         <div className="flex items-center gap-8 md:gap-12">
           <div className="flex items-center gap-3 text-slate-900 dark:text-white">
             <div className="size-8 text-primary">
@@ -20,24 +26,26 @@ export default function Nav() {
           </div>
         </div>
         <div className="flex items-center justify-end gap-4 md:gap-6 flex-1">
-          <label className="hidden md:flex flex-col min-w-40 h-10 max-w-64 flex-1">
-            <Input
-              className="w-full border-none bg-slate-100 dark:bg-[#282e39] text-slate-900 dark:text-white focus:ring-0 placeholder:text-slate-400 dark:placeholder:text-text-secondary text-sm"
-              placeholder={t("searchPlaceholder")}
-            />
-          </label>
+          <div className="hidden md:flex flex-col min-w-40 h-10 max-w-64 flex-1">
+            <SearchInput />
+          </div>
           <div className="flex gap-3">
-            <Link
-              href="/cart"
-              className="hidden md:flex items-center justify-center rounded-full size-10 bg-slate-100 dark:bg-[#282e39] hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors text-slate-900 dark:text-white"
+            <Button
+              onClick={() => setMobileSearch(true)}
+              variant="secondary"
+              className="rounded-full lg:hidden"
             >
-              <ShoppingCart className="size-4" />
+              <SearchIcon className="size-4" />
+            </Button>
+            <Link href="/cart" className="hidden lg:block">
+              <Button size="icon" variant="secondary" className="rounded-full">
+                <ShoppingCart className="size-4" />
+              </Button>
             </Link>
-            <Link
-              href="/profile"
-              className="hidden md:flex items-center justify-center rounded-full size-10 bg-slate-100 dark:bg-[#282e39] hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors text-slate-900 dark:text-white"
-            >
-              <UserIcon className="size-4" />
+            <Link href="/profile" className="hidden lg:block">
+              <Button size="icon" variant="secondary" className="rounded-full">
+                <UserIcon className="size-4" />
+              </Button>
             </Link>
           </div>
         </div>

bb030cda26475c998306c266c3a1bbd8a65dff2c
yspv <yusupovamirxon@yahoo.com>
2025-12-31 11:23:21 +0500

    empty cart localization

--- src/modules/cart/templates/index.tsx
+++ src/modules/cart/templates/index.tsx
@@ -1,18 +1,20 @@
 import { HttpTypes } from "@medusajs/types";
 import CartItem from "../components/cart-item";
 import { Summary } from "./summary";
+import { ShoppingCart } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
 
 type Props = {
   cart?: HttpTypes.StoreCart | null;
 };
 
 export default function CartTemplate({ cart }: Props) {
-  if (!cart) {
-    return null;
-  }
+  const t = useTranslations("Cart");
   return (
     <div className="flex flex-col min-h-screen flex-1">
-      {!!cart.items ? (
+      {!!cart?.items?.length ? (
         <div className="flex flex-col lg:flex-row gap-8 lg:gap-12 relative">
           {/* Left Column: Cart Items */}
           <section className="flex-1 flex flex-col gap-6">
@@ -20,7 +22,6 @@ export default function CartTemplate({ cart }: Props) {
               <CartItem key={item.id} item={item} />
             ))}
           </section>
-
           {/* Right Column: Order Summary */}
           <aside className="w-full lg:w-[380px] shrink-0">
             <Summary cart={cart} />
@@ -29,20 +30,17 @@ export default function CartTemplate({ cart }: Props) {
       ) : (
         <div className="py-20 flex flex-col items-center justify-center text-center animate-in zoom-in-95 duration-500">
           <div className="size-24 rounded-full bg-gray-100 dark:bg-gray-800 flex items-center justify-center text-text-muted mb-6">
-            <span className="material-symbols-outlined text-4xl">
-              shopping_cart_off
-            </span>
+            <ShoppingCart className="size-8" />
           </div>
           <h2 className="text-2xl font-bold text-text-main dark:text-white mb-2">
-            Your cart is empty
+            {t("empty.title")}
           </h2>
           <p className="text-text-muted mb-8 max-w-sm">
-            Looks like you havent added anything to your cart yet. Check out our
-            latest collections!
+            {t("empty.description")}
           </p>
-          <button className="bg-primary text-white px-8 py-3 rounded-lg font-bold hover:bg-primary/90 transition-all">
-            Start Shopping
-          </button>
+          <Link href="/">
+            <Button>{t("empty.startShopping")}</Button>
+          </Link>
         </div>
       )}
     </div>

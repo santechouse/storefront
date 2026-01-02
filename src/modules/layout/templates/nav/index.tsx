9e26293e759dd7707d5167f56ab62c1d901131e4
yspv <yusupovamirxon@yahoo.com>
2026-01-02 13:23:39 +0500

    nav add catalog item

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -6,8 +6,10 @@ import React from "react";
 import { SearchInput } from "../../components/search-input";
 import { MobileSearchInput } from "../../components/mobile-search-input";
 import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
 
 export default function Nav() {
+  const t = useTranslations("Nav");
   const [isMobileSearch, setMobileSearch] = React.useState(false);
   if (isMobileSearch) {
     return <MobileSearchInput onClose={() => setMobileSearch(false)} />;
@@ -16,14 +18,25 @@ export default function Nav() {
     <div className=" sticky top-0 z-50 w-full px-4 py-4 md:px-6 lg:px-8 flex justify-center backdrop-blur-md bg-white/80 dark:bg-background/80 border-b border-slate-200 dark:border-[#282e39]/50">
       <div className="flex w-full items-center justify-between gap-4">
         <div className="flex items-center gap-8 md:gap-12">
-          <div className="flex items-center gap-3 text-slate-900 dark:text-white">
+          <Link
+            href="/"
+            className="flex items-center gap-3 text-slate-900 dark:text-white"
+          >
             <div className="size-8 text-primary">
               <Image src="/logo.svg" width={40} height={40} alt="Logo" />
             </div>
             <h2 className="text-xl font-bold leading-tight tracking-[-0.015em]">
               Santechouse
             </h2>
-          </div>
+          </Link>
+          <nav className="hidden lg:flex items-center gap-8">
+            <Link
+              className="text-sm bg-secondary px-3 py-1.5 rounded-lg font-medium hover:text-primary transition-colors"
+              href="/catalog"
+            >
+              {t("catalog")}
+            </Link>
+          </nav>
         </div>
         <div className="flex items-center justify-end gap-4 md:gap-6 flex-1">
           <div className="hidden md:flex flex-col min-w-40 h-10 max-w-64 flex-1">

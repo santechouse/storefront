0348d0e0968de0303d50cfb61747fe74360ce934
yspv <yusupovamirxon@yahoo.com>
2026-01-03 12:17:50 +0500

    packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ const eslintConfig = defineConfig([
     rules: {
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
     },
   },
 ]);
--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,26 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000", // Specify the port your local API or server is running on
+        pathname: "/**", // Allows any path
+      },
+      {
+        protocol: "https",
+        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.santechouse.uz",
+      },
+    ],
+    dangerouslyAllowLocalIP: true,
+    dangerouslyAllowSVG: true,
+  },
 };
 
 export default withNextIntl(nextConfig);

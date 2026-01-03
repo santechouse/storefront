da9ad2a0ef0ab50ba84efdd9c0f194b8568a5871
yspv <yusupovamirxon@yahoo.com>
2026-01-03 11:28:37 +0500

    home page

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import { hasLocale, NextIntlClientProvider, useTranslations } from "next-intl";
+import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 import { ThemeProvider } from "next-themes";
 import Nav from "@/modules/layout/templates/nav";
 import BottomNav from "@/modules/layout/templates/bottom-nav";
+import Footer from "@/modules/layout/templates/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,7 @@ export function generateStaticParams() {
 
 export async function generateMetadata(
   props: Omit<LayoutProps<"/[locale]">, "children">,
-) {
+): Promise<Metadata> {
   const { locale } = await props.params;
   const t = await getTranslations({
     locale,
@@ -56,11 +57,16 @@ export default async function LocaleLayout({
   return (
     <html className="h-full" lang={locale} suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} dark antialiased  font-display text-slate-900 dark:text-white transition-colors duration-300`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-display transition-colors duration-300`}
       >
         <NextIntlClientProvider>
           <Nav />
-          <ThemeProvider>{children}</ThemeProvider>
+          <ThemeProvider>
+            <main className="flex-1 flex flex-col justify-center lg:flex-row max-w-360 mx-auto w-full px-4 md:px-6 lg:px-8 py-6 gap-8">
+              {children}
+            </main>
+          </ThemeProvider>
+          <Footer />
           <BottomNav />
         </NextIntlClientProvider>
       </body>
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-  return <></>;
+import { listCollections } from "@/lib/data/collections";
+import { getRegion } from "@/lib/data/regions";
+import FeaturedBrands from "@/modules/home/components/featured-brands";
+import FeaturedProducts from "@/modules/home/components/featured-products";
+import Hero from "@/modules/home/components/hero";
+
+export default async function Page({ params }: PageProps<"/[locale]">) {
+  const { locale } = await params;
+  const region = await getRegion("uz");
+
+  const { collections } = await listCollections(locale);
+
+  if (!region || !collections) {
+    return null;
+  }
+
+  return (
+    <div className="w-full flex flex-col gap-8 md:gap-12">
+      <Hero />
+      <FeaturedBrands />
+      <FeaturedProducts
+        locale={locale}
+        region={region}
+        collections={collections}
+      />
+    </div>
+  );
 }

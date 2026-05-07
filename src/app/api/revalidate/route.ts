63486c760dc2369e4f6980899428d380ea076ebb
yspv <yusupovamirxon@yahoo.com>
2026-05-07 08:43:23 +0500

    fix: revalidate endpoint with with security key check

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,22 +1,30 @@
-import { NextRequest, NextResponse } from "next/server"
-import { revalidateTag } from "next/cache"
+import { NextRequest, NextResponse } from "next/server";
+import { revalidateTag } from "next/cache";
 
 export async function GET(req: NextRequest) {
-  const searchParams = req.nextUrl.searchParams
-  const tags = searchParams.get("tags")
+  const searchParams = req.nextUrl.searchParams;
+  const secret = searchParams.get("secret");
+  const tags = searchParams.get("tags");
 
-  if (!tags) {
-    return NextResponse.json({ error: "No tags provided" }, { status: 400 })
+  if (
+    process.env.REVALIDATE_SECRET &&
+    secret !== process.env.REVALIDATE_SECRET
+  ) {
+    return NextResponse.json({ error: "Invalid secret" }, { status: 401 });
   }
 
-  const tagsArray = tags.split(",")
+  if (!tags) {
+    return NextResponse.json({ error: "No tags provided" }, { status: 400 });
+  }
+
+  const tagsArray = tags.split(",");
 
   tagsArray.forEach((tag) => {
-    revalidateTag(tag, {})
-  })
+    revalidateTag(tag, "max");
+  });
 
   return NextResponse.json(
     { message: "Revalidated", tags: tagsArray },
-    { status: 200 }
-  )
+    { status: 200 },
+  );
 }
